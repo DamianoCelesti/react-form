3ff89da0df26ff3f8ab203de2a198d898af1de5c
DamianoCelesti <damiano.celesti@gmail.com>
2025-02-18 17:21:38 +0100

    added form

--- src/components/ArticoliLista.jsx
+++ src/components/ArticoliLista.jsx
@@ -1,15 +1,36 @@
+import { useState } from 'react'
 
 
-const articoli = ["html", "css", "java", "react"];
+const articoliIniziali = ["html", "css", "java", "react"];
 
 
 export default function ArticoliLista() {
 
+    const [articoli, setArticoli] = useState(articoliIniziali);
+    const [nuovoTitolo, setNuovoTitolo] = useState('');
+
+    const addTitolo = event => {
+        event.preventDefault();
+        const listaAggiornata = [...articoli, nuovoTitolo];
+        setArticoli(listaAggiornata);
+        setNuovoTitolo("");
+
+    }
     return (
-        <ul>
-            {articoli.map((articolo, i) => (
-                <li key={i}>{articolo}</li>
-            ))}
-        </ul>
+        <>
+            <form onSubmit={addTitolo}>
+                <input
+                    type="text"
+                    value={nuovoTitolo}
+                    onChange={event => { setNuovoTitolo(event.target.value) }}
+                />
+                <button>Invia</button>
+            </form>
+            <ul>
+                {articoli.map((articolo, i) => (
+                    <li key={i}>{articolo}</li>
+                ))}
+            </ul>
+        </>
     )
 }
